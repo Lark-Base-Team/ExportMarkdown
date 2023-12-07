986f1de0b8200cefaf5d9e677d5a8249abe18972
Alice Liddell <alice@loli.co>
2023-12-07 20:13:15 +0800

    feat: upload bitable exporter

--- README.md
+++ README.md
@@ -1,7 +1,60 @@
-# Base JS SDK DEMO
-## node: 16+
-1. npm install -g yarn
-2. yarn
-3. yarn run start
+# Bitable Exporter
 
-Now, you can see the output URL in the console, please click, then open the page, and copy the URL
+Bitable Exporter is a React application designed to export data from tables and views in the Bitable platform to Markdown format. It utilizes the Semi-UI library for UI components and the Lark Base Open JavaScript SDK for interacting with tables and views.
+
+## Features
+
+- Export visible records to Markdown.
+- Export selected records to Markdown.
+- Display exported Markdown content in a TextArea.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Node.js and npm installed.
+- Access to the Bitable platform.
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/blinglbingdev/bitable-exporter.git
+```
+
+2. Install dependencies:
+
+```bash
+cd bitable-exporter
+yarn
+```
+
+## Usage
+
+1. Start the development server:
+
+```bash
+yarn dev
+```
+
+This will launch the application in your default web browser.
+
+2. Follow instructions [here](https://www.feishu.cn/hc/zh-CN/articles/948392023042-%E4%BD%BF%E7%94%A8%E5%A4%9A%E7%BB%B4%E8%A1%A8%E6%A0%BC%E6%8F%92%E4%BB%B6) to export visible or selected records to Markdown.
+
+## Configuration
+
+* If needed, update the Bitable SDK configuration in the code.
+* Adjust i18n localization settings in locales/i18n.js.
+
+## Acknowledgments
+
+* Semi-UI
+* Bitable SDK
+* json2md
+* React
+* ReactDOM
+
+## Contact
+
+For issues or inquiries, please open an issue on the GitHub repository.
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,174 @@
-import React, { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom/client'
-import { bitable, IAttachmentField } from '@lark-base-open/js-sdk';
-import { Alert, AlertProps } from 'antd';
+import { Button, Spin, TextArea } from "@douyinfe/semi-ui";
+import { ITable, IView, ToastType, bitable } from "@lark-base-open/js-sdk";
+import json2md from "json2md";
+import React, { useCallback, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import i18n from "./locales/i18n";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <LoadApp/>
-  </React.StrictMode>
-)
+/**
+ * LoadApp component for exporting table data to Markdown format.
+ */
+const LoadApp = () => {
+  // State variables
+  const [isReady, setIsReady] = useState(false);
+  const [isLoadingVisible, setIsLoadingVisible] = useState(false);
+  const [isLoadingSelected, setIsLoadingSelected] = useState(false);
+  const [md, setMD] = useState("");
 
-function LoadApp() {
-  const [info, setInfo] = useState('get table name, please waiting ....');
-  const [alertType, setAlertType] = useState<AlertProps['type']>('info');
+  // Initialize the component
   useEffect(() => {
-    const fn = async () => {
-      const table = await bitable.base.getActiveTable();
-      const tableName = await table.getName();
-      setInfo(`The table Name is ${tableName}`);
-      setAlertType('success');
+    const f = async () => {
+      // Fetch active table data asynchronously
+      await bitable.base.getActiveTable();
+      setIsReady(true);
     };
-    fn();
+    f();
   }, []);
 
-  return <div>
-    <Alert message={info} type={alertType} />
-  </div>
-}
+  /**
+   * Export table data to Markdown format.
+   * @param {Object} options - Export options.
+   * @param {ITable} options.table - The table to export.
+   * @param {IView} options.view - The view to export.
+   * @param {string[]} options.recordIdList - List of record IDs to export.
+   */
+  const exportToMarkDown = useCallback(
+    async ({
+      table,
+      view,
+      recordIdList,
+    }: {
+      table?: ITable;
+      view?: IView;
+      recordIdList: (string | undefined)[];
+    }) => {
+      if (!table) {
+        table = await bitable.base.getActiveTable();
+      }
+      if (!view) {
+        view = await table.getActiveView();
+      }
+      // Get field metadata for the view
+      const fieldMetaList = await view.getFieldMetaList();
+
+      // Prepare table headers
+      const headers = fieldMetaList.map((f) => f.name);
+      const rows = [];
+
+      // Iterate over recordIdList and fetch cell data
+      for (const recordId of recordIdList) {
+        if (!recordId) {
+          continue;
+        }
+        const row: string[] = [];
+        for (const fieldMeta of fieldMetaList) {
+          const field = await table.getFieldById(fieldMeta.id);
+          let cellString = await field.getCellString(recordId);
+          row.push(cellString);
+        }
+        rows.push(row);
+      }
+
+      // Convert data to Markdown format
+      setMD(json2md([{ table: { headers, rows } }]));
+    },
+    []
+  );
+
+  /**
+   * Export visible table data to Markdown format.
+   */
+  const exportVisible = useCallback(async () => {
+    setIsLoadingVisible(true);
+    try {
+      const table = await bitable.base.getActiveTable();
+      const view = await table.getActiveView();
+      const recordIdList = await view.getVisibleRecordIdList();
+      if (!recordIdList) {
+        return;
+      }
+      await exportToMarkDown({ table, view, recordIdList });
+    } finally {
+      setIsLoadingVisible(false);
+    }
+  }, []);
+
+  /**
+   * Export selected table data to Markdown format.
+   */
+  const exportSelected = useCallback(async () => {
+    setIsLoadingSelected(true);
+    try {
+      const { tableId, viewId } = await bitable.base.getSelection();
+      if (!tableId || !viewId) {
+        await bitable.ui.showToast({
+          toastType: ToastType.error,
+          message: i18n.t("errorMsgGetSelectionFailed"),
+        });
+        return;
+      }
+      const recordIdList = await bitable.ui.selectRecordIdList(tableId, viewId);
+      await exportToMarkDown({ recordIdList });
+    } finally {
+      setIsLoadingSelected(false);
+    }
+  }, []);
+
+  // Render loading screen if not ready
+  if (!isReady) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 8,
+        }}
+      >
+        <div>
+          <Spin size="middle" />
+        </div>
+        <div>{i18n.t("initializingText")}</div>
+      </div>
+    );
+  }
+
+  // Render the main component
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        gap: 8,
+      }}
+    >
+      <h4>{i18n.t("title")}</h4>
+      <Button
+        theme="solid"
+        loading={isLoadingVisible}
+        disabled={isLoadingSelected}
+        onClick={exportVisible}
+      >
+        {i18n.t("exportVisibleButtonText")}
+      </Button>
+      <Button
+        theme="solid"
+        loading={isLoadingSelected}
+        disabled={isLoadingVisible}
+        onClick={exportSelected}
+      >
+        {i18n.t("exportSelectedButtonText")}
+      </Button>
+      {md && <TextArea value={md} autosize />}
+    </div>
+  );
+};
+
+// Render the component to the root element
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <LoadApp />
+  </React.StrictMode>
+);
